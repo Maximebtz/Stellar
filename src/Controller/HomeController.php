fd63e36455cc1fc466a784ace1b110ee2bb77e00
Maximebtz <120190748+Maximebtz@users.noreply.github.com>
2023-09-30 13:58:40 +0200

    Update : CSS home Page, Map popup OK

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -20,22 +20,28 @@ class HomeController extends AbstractController
 
     #[Route('/home', name: 'app_home')]
     public function index(Request $request): Response
-    {
-        $page = filter_var($request->query->get('page', 1), FILTER_VALIDATE_INT, ['options' => ['default' => 1, 'min_range' => 1]]);
-        $perPage = 20;
+{
+    $perPage = 24;
+    $totalAdverts = $this->entityManager->getRepository(Advert::class)
+        ->getTotalAdvertsCount();
 
-        $adverts = $this->entityManager->getRepository(Advert::class)
-            ->findPaginatedAdverts($page, $perPage);
+    // Calculez le nombre total de pages nécessaires en fonction du nombre total d'annonces et du nombre par page
+    $totalPages = ceil($totalAdverts / $perPage);
 
-        $totalAdverts = $this->entityManager->getRepository(Advert::class)
-            ->getTotalAdvertsCount();
+    // Récupérez le numéro de la page à partir de la requête
+    $page = filter_var($request->query->get('page', 1), FILTER_VALIDATE_INT, ['options' => ['default' => 1, 'min_range' => 1, 'max_range' => $totalPages]]);
 
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-            'adverts' => $adverts,
-            'totalAdverts' => $totalAdverts,
-            'page' => $page,
-            'perPage' => $perPage,
-        ]);
-    }
+    // Récupérez les annonces pour la page actuelle
+    $adverts = $this->entityManager->getRepository(Advert::class)
+        ->findPaginatedAdverts($page, $perPage);
+
+    return $this->render('home/index.html.twig', [
+        'controller_name' => 'HomeController',
+        'adverts' => $adverts,
+        'totalAdverts' => $totalAdverts,
+        'page' => $page,
+        'perPage' => $perPage,
+        'totalPages' => $totalPages, 
+    ]);
+}
 }
